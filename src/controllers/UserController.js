b1d3c70eaccadf7ef9aaf2551268ed0cce03e7ed
Jack Zhang <15jzhang@gmail.com>
2016-06-30 12:35:49 -0700

    Refactor user schema and implement UserController methods for get/post user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,7 +1,35 @@
 const { User } = require('./../models');
 
 const getUser = (req, res) => {
-  const facebookId = req.query.facebookId;
+  User.fetch({ facebook_id: req.query.facebook_id })
+    .then(fetchedUser => {
+      res.status(200).json(fetchedUser);
+    })
+    .catch(err => {
+      console.err(err);
+    });
 };
 
-module.exports = { getUser };
+const postUser = (req, res) => {
+  const newUser = {
+    facebook_id: req.body.facebook_id,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    display_name: req.body.display_name,
+    gender: req.body.gender,
+    photo_url: req.body.photo_url,
+  };
+
+  User.save(newUser)
+    .then(savedUser => {
+      res.status(200).send(savedUser);
+    })
+    .catch(err => {
+      console.err(err);
+    });
+};
+
+module.exports = {
+  getUser,
+  postUser,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const UserController = require('./../controllers/UserController');
 
 const routes = (app) => {
   app.get('/db/user', UserController.getUser);
+  app.post('/db/user', UserController.postUser);
 };
 
 module.exports = routes;
