283951fe9b7ddd368a7a92521f44ccbffbb0b3fc
Leo Adelstein <leoadelstein@gmail.com>
2016-07-02 20:32:55 -0700

    loading up sql schema on server start

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ if (process.env.NODE_ENV === 'development') {
   dotenv.config({ path: './env/production.env' });
 }
 
+// load up schema
+require('./models');
+
 const express = require('express');
 const app = express();
 
