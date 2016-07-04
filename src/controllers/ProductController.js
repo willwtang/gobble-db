22545ce704af33e350c6a93dbb7422803e88c8ba
Leo Adelstein <leoadelstein@gmail.com>
2016-07-04 15:18:55 -0700

    added get product function

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,4 +1,5 @@
 const { Product, Category, ProductCategory, Tag, ProductTag, Ingredient, ProductIngredient, Media } = require('./../models');
+const QueryBuilder = require('../orm/querybuilder');
 
 const addCategories = (upc, categories) => {
   for (let i = 0; i < categories.length; i++) {
@@ -137,6 +138,112 @@ const postProduct = (req, res) => {
   res.end();
 };
 
+// const getPostsByFriends = function(date, limit, user) {
+//   const qb = new QueryBuilder();
+//   const qb2 = new QueryBuilder();
+//   qb2.select({ what: 'followed', from: 'Follow', where: `Follow.follower = ${user}`, as: 'T1' });
+//   return (qb
+//     .select({ what: '*', from: qb2.materialize() })
+//     .innerJoin({ target: 'User', on: { 'User.facebook_id': 'T1.followed' } })
+//     .innerJoin({ target: 'Post', on: `Post.parentId IS NULL AND T1.followed = Post.User_facebook_id AND date(Post.Post_created_at) < date('${date}')` })
+//     .leftJoin({ target: 'Product', on: { 'Post.Product_upc': 'Product.upc' } })
+//     .orderBy('Post_created_at')
+//     .fire()
+//     .then(res => res.slice(0, limit + 1)));
+// };
+// select * from category t1 inner join product_category t2 on t1.id = t2.Category_id
+//     -> where Product_upc = 22000011879;
+
+const getProduct = (req, res) => {
+  const upc = req.query.upc;
+  let tasksLeft = 4;
+
+  const toReturn = {};
+  toReturn.categories = [];
+  toReturn.tags = [];
+  toReturn.ingredients = [];
+
+  const productQB = new QueryBuilder();
+  const categoriesQB = new QueryBuilder();
+  const tagsQB = new QueryBuilder();
+  const ingredientsQB = new QueryBuilder();
+
+  productQB.select({ what: '*', from: 'Product', where: `Product.upc = ${upc}` });
+  console.log(productQB.materialize());
+  productQB.fire()
+    .then((result) => {
+      toReturn.product = result[0];
+      if (--tasksLeft === 0) {
+        console.log(toReturn);
+        res.end(JSON.stringify(toReturn));
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  //
+  categoriesQB.select({ what: 'Category.name', from: 'Category' })
+    .innerJoin({ target: 'product_category', on: { 'product_category.Category_id': 'category.id' } })
+    .where({ Product_upc: upc });
+
+  console.log(categoriesQB.materialize());
+  categoriesQB.fire()
+    .then((result) => {
+      for (let i = 0; i < result.length; i++) {
+        toReturn.categories.push(result[i].name);
+      }
+      if (--tasksLeft === 0) {
+        console.log(toReturn);
+        res.end(JSON.stringify(toReturn));
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  //
+  tagsQB.select({ what: 'Tag.name', from: 'Tag' })
+    .innerJoin({ target: 'product_tag', on: { 'product_tag.Tag_id': 'tag.id' } })
+    .where({ Product_upc: upc });
+
+  console.log(tagsQB.materialize());
+  tagsQB.fire()
+    .then((result) => {
+      for (let i = 0; i < result.length; i++) {
+        toReturn.tags.push(result[i].name);
+      }
+      if (--tasksLeft === 0) {
+        console.log(toReturn);
+        res.end(JSON.stringify(toReturn));
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  //
+  ingredientsQB.select({ what: 'Ingredient.name', from: 'Ingredient' })
+    .innerJoin({ target: 'product_ingredient', on: { 'product_ingredient.Ingredient_id': 'Ingredient.id' } })
+    .where({ Product_upc: upc });
+
+  console.log(ingredientsQB.materialize());
+  ingredientsQB.fire()
+    .then((result) => {
+      for (let i = 0; i < result.length; i++) {
+        toReturn.ingredients.push(result[i].name);
+      }
+      if (--tasksLeft === 0) {
+        console.log(toReturn);
+        res.end(JSON.stringify(toReturn));
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+// getProduct(22000011879)
 module.exports = {
-  postProduct,
+  postProduct, getProduct,
 };
